04635290aa55d8bc9258a995525a24c5e5996030
mostafa <mostafamansour163@yahoo.com>
2021-08-31 20:09:18 +0200

    userLogin and init_UserRegister

--- app/src/main/java/com/authentication/register/RegisterViewModel.java
+++ app/src/main/java/com/authentication/register/RegisterViewModel.java
@@ -1,7 +1,17 @@
 package com.authentication.register;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterViewModel extends ViewModel
 {
+    private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    private DatabaseReference userRef = FirebaseDatabase.getInstance().getReference();
+    public MutableLiveData<String> stringMutableLiveData = new MutableLiveData<>();
+
+
 }
